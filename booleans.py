d813fc4bdf6059a1b607b4796e69d14b852bdf85
EddieWadors <wador101@mail.chapman.edu>
2016-03-10 23:12:11 -0800

    homework

---
+++ booleans.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+File: booleans.py
+Copyright (c) 2016 Eddie Wadors
+License: MIT
+- Defining code into boolean answers.
+"""
+c = 41  #c is equal to 41.
+c == 40  #c equals 40, which is false considering python understands c to be 41.
+c != 40 and c < 41  #c is not equal to 40 and c is less than 41.
+c != 41 or c < 41  #c is not equal to 41 or c is less than 41.
+not c == 40  #c is not equal to 40.
+not c > 40  #c is not greater than 40.
+c <= 41  #c is less than or equal to 41.
+not False  #True.
+True and False  #c can be True and False.
+False or True  #c can be True or False.
+False or False or False  #c is False in every aspect.
+True and True and False  #c can be True and False.
+False == 0  #0 is False.
+True == 0  #0 is True.
+True == 1  #1 is True.
